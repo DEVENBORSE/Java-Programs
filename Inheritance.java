305d1de7568fdac3bcbe777d77b9ba51a0a96d46
Deven Anil Borse <135968432+DEVENBORSE@users.noreply.github.com>
2025-07-28 03:33:54 +0000

    deven

---
+++ Inheritance.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Vehicle{
+    void startEngine(){
+        System.out.println("The Engione started of Ertiga")
+    }
+}
+
+class Ertiga extends Vehicle{
+    void 
+} 
