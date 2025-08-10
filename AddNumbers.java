38819cb283bdc1183880c2e04beb44121777aa5b
Deven Borse <devenborse37@gmail.com>
2025-08-10 17:45:38 +0530

    its a Interface program

--- AddNumbers.java
+++ AddNumbers.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 
 public class AddNumbers{
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("enter the first number:");
-        int a = sc.nextInt();
-        System.out.println("enter the second number:");
-        int b = sc.nextInt();
-        System.out.println("enter the third number:");
-        int c = sc.nextInt();
-        int sum = a + b + c ;
-        System.out.println("sum of three numbers is: "+sum);
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers do you want to add? ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Enter number " + i + ": ");
+            sum += sc.nextInt();
+        }
+        double average = (n > 0) ? (double) sum / n : 0;
+        System.out.println("Sum of " + n + " numbers is: " + sum);
+        System.out.println("Average is: " + average);
+        
     }
-}
+
 //for importing the full string:
 // String str= sc.nextLine();
 // System.out.println(str);
